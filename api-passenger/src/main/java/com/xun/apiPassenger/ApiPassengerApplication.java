9d82e7b6056ce1298fde382b6d613c60a3fb1c2e
shilixun <shilixun@sinosoft.com.cn>
2023-04-07 14:16:05 +0800

    验证码服务创建与注册

--- api-passenger/src/main/java/com/xun/apiPassenger/ApiPassengerApplication.java
+++ api-passenger/src/main/java/com/xun/apiPassenger/ApiPassengerApplication.java
@@ -2,6 +2,7 @@ package com.xun.apiPassenger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author slx
@@ -11,6 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @version: 1.0
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ApiPassengerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiPassengerApplication.class);
