8b3c312f46bb440ab4bcf999d114a2aaebfa7f63
shilixun <shilixun@sinosoft.com.cn>
2023-04-07 10:42:38 +0800

    随心出行项目

---
+++ api-passenger/src/main/java/com/xun/apiPassenger/ApiPassengerApplication.java
@@ -0,0 +1,18 @@
+package com.xun.apiPassenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author slx
+ * @ClassName ApiPassengerApplication
+ * @description: TODO
+ * @date 2023年04月07日
+ * @version: 1.0
+ */
+@SpringBootApplication
+public class ApiPassengerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiPassengerApplication.class);
+    }
+}
